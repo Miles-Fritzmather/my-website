4cd07e27b879735de3e216b65f62ec80bc3d7d83
Miles F <miles.fritzmather@gmail.com>
2026-02-18 19:04:15 -0600

    fixing directing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 const Page = () => {
   useEffect(() => {
-    window.location.href = "/about";
+    window.location.href = "./about";
   }, []);
   return null;
 };
